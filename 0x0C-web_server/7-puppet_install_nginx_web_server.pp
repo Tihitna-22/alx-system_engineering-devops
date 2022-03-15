ea07eb260b570ea816a5d196d1813c784808f0f1
tihitna-22 <tihitna222sisay@gmail.com>
2022-03-15 12:40:58 -0700

    add 7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+# Install nginx web server
+
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+
+# Index page
+file { '/var/www/html/index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!'
+}
+
+# Redirection
+file_line { 'Redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => '        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['Redirection']
+}
